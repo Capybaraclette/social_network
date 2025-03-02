a1e97cfddea914d56764887762a62e9085dea0c3
TOTEL Dimitri <dimitri.totel@gmail.com>
2025-03-02 15:03:20 +0100

    sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvider"
 import Navbar from "@/components/ui/Navbar";
+import Sidebar from "@/components/Sidebar";
+
 
 
 const geistSans = localFont({
@@ -43,7 +45,9 @@ export default function RootLayout({
                 {/*container to center content*/}
                 <div className={"max-w-7xl mx-auto px-4"}>
                   <div className={"grid grid-cols-1 lg:grid-cols-12 gap-6"}>
-                    <div className={"hidden lg:block lg:col-span-3"}>sidebar</div>
+                    <div className={"hidden lg:block lg:col-span-3"}>
+                      <Sidebar/>
+                    </div>
                     <div className={"lg:col-span-9"}>{children}</div>
                   </div>
                 </div>
