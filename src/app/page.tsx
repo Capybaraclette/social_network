fb881043df713aba85406d81d0f0a4448573fc10
TOTEL Dimitri <dimitri.totel@gmail.com>
2025-03-02 11:52:18 +0100

    clek

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
       <div>
         <h1>Hello, please send help i am bored as fuckkkkk, i don't like web, and i am forced to do a 6 hour tutorial before tomorow.</h1>
+
       </div>
 
   );
