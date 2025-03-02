97f0a42d4942bca029c630a8724b6dc187f63170
TOTEL Dimitri <dimitri.totel@gmail.com>
2025-03-02 12:44:27 +0100

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 import Image from "next/image";
 import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
 import {Button} from "@/components/ui/button";
-import ModeToggle from "@/components/ui/ModeToggle";
+
 
 export default function Home() {
   return (
       <div className={"m-4"}>
-          <SignedOut>
-              <SignInButton mode={"modal"} >
-                <Button>
-                    Sign in
-                </Button>
-              </SignInButton >
-          </SignedOut>
-    <ModeToggle></ModeToggle>
-          <SignedIn>
-              <UserButton />
-          </SignedIn>
+        <h1>Home page content</h1>
       </div>
 
   );
